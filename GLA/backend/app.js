e4313234df7897343d3e3cc92e32ab6cf4bb2da2
koushaljhacs <koushaljha.cs@gmail.com>
2026-03-12 13:40:56 +0530

    product route created

--- GLA/backend/app.js
+++ GLA/backend/app.js
@@ -5,6 +5,11 @@ let path = require('path');
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, 'views'));
 
+let products = ["phone", "watch", "earbuds"];
+
+app.get('/product', (req, res) => {
+    res.render("show", {products})
+})
 app.listen(4000, () => {
-    console.log("server is running at port 4000");
+    console.log("server is running at port 4000: localhost:4000");
 });
