d1967c975d571c14071e42d693b3f8ec8808743f
koushaljhacs <koushaljha.cs@gmail.com>
2026-03-12 14:05:51 +0530

    product added by post

--- GLA/backend/app.js
+++ GLA/backend/app.js
@@ -9,7 +9,18 @@ let products = ["phone", "watch", "earbuds"];
 
 app.get('/product', (req, res) => {
     res.render("show", {products})
-})
+});
+
+app.get('/add', (req, res) => {
+    res.render("add");
+});
+
+app.get('/submit', (req, res) => {
+    console.log(req.query);
+    products.push(req.query.p_name);
+    res.redirect('/product');
+});
+
 app.listen(4000, () => {
     console.log("server is running at port 4000: localhost:4000");
 });
