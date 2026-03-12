9da125bc0762e87d605f2ab2dd3c3d4e16981359
koushaljhacs <koushaljha.cs@gmail.com>
2026-03-12 13:24:15 +0530

    view engine path set

--- GLA/backend/app.js
+++ GLA/backend/app.js
@@ -1,5 +1,9 @@
 let express = require('express');
 let app = express();
+let path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'views'));
 
 app.listen(4000, () => {
     console.log("server is running at port 4000");
